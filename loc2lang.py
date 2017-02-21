0dd8d87480fbaa21e659c6d4261e659901716a35
afshinrahimi <afshinrahimi@gmail.com>
2017-02-21 11:46:32 +1100

    init commit of loc2lang model

---
+++ loc2lang.py
@@ -0,0 +1,11 @@
+'''
+Created on 21 Feb 2017
+Given a training set of lat/lon as input and probability distribution over words as output,
+train a model that can predict words based on location.
+then try to visualise borders and regions (e.g. try many lat/lon as input and get the probability of word yinz
+in the output and visualise that).
+@author: af
+'''
+
+if __name__ == '__main__':
+    
